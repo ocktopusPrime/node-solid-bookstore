b4179dee79806dc66e9aaa27a92015490fdb9d6d
jared.long <jared.long.d@gmail.com>
2022-06-14 20:06:45 -0600

    trying a new config for deployment

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+// Setup express app
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.listen(port, () => console.log(`Server up and running on ${port}.`));
+// Specify the Port where the backend server can be accessed and start listening on that port
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port}.`));
