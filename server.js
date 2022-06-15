3b432c4df02ace2eb2b35e19a263ec4bb7952775
jared.long <jared.long.d@gmail.com>
2022-06-14 21:08:33 -0600

    updated express.static location

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port}.`));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static('client/dist'));
 
 	app.get('*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
