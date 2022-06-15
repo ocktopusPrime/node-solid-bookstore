b247780cafb759ad67e06940ebbf3f0f2ab99f0f
jared.long <jared.long.d@gmail.com>
2022-06-14 20:38:02 -0600

    changed path resolution

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
 	);
 }
