37bbc20f39e51fd60af958d81094d3002771da94
jared.long <jared.long.d@gmail.com>
2022-06-14 20:11:00 -0600

    updated package.json

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ app.use(cors());
 // Specify the Port where the backend server can be accessed and start listening on that port
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port}.`));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
