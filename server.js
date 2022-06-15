1929962336ab4a3f7ef041f5e3c179f09aca9d32
jared.long <jared.long.d@gmail.com>
2022-06-14 20:17:58 -0600

    tryin something else

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -17,7 +16,7 @@ app.listen(port, () => console.log(`Server up and running on port ${port}.`));
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	);
+	// app.get('*', (req, res) =>
+	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	// );
 }
