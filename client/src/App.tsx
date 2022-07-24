2964c8fa385148a1ed77fd4a96bc07e7ee40053f
jared.long <jared.long.d@gmail.com>
2022-07-24 11:37:41 -0600

    Updated the mongo URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { Bookstore } from './Components/Bookstore';
 
 export const mongo =
 	process.env.NODE_ENV === 'production'
-		? 'https://charts.mongodb.com/charts-bookstore-jglhi/embed/charts?id=62d844b9-f1eb-4b9c-8f8f-568118f260e6&maxDataAge=60&theme=dark&autoRefresh=true'
+		? 'https://bookstore-green-team.herokuapp.com/'
 		: 'http://localhost:5000';
 
 const App: Component = () => {
